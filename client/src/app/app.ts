2cbad8e383ccc85bd818345e123365312b22258a
r.tohidifar <r.tohidifar@N-RTOHIDIFAR>
2025-11-11 12:42:25 +0330

    data for dating app

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnChanges, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit {
+  
+  protected readonly title = signal('DatingApp');
+  public http = inject(HttpClient);
+  public users: any;
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:5000/api/users').subscribe({
+      next: Response => this.users = Response,
+      error: error => console.log(error),
+      complete:  () => console.log("done")
+    })
+  }
 }
